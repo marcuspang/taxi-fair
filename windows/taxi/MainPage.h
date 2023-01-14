a2f8fb1db2aa68b552f9d3f2c85ccf58bd8e2f4e
Marcus Pang <marcuspangyuyang@gmail.com>
2023-01-14 13:50:26 +0800

    Initial

---
+++ windows/taxi/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::taxi::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::taxi::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
