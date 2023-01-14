65d946528e00732338dc632a144d3a3d85e646d4
Marcus Pang <marcuspangyuyang@gmail.com>
2023-01-14 18:01:41 +0800

    feat: add map to main page

--- App.tsx
+++ App.tsx
@@ -1,55 +1,17 @@
 import React from 'react';
-import type { PropsWithChildren } from 'react';
 import {
   SafeAreaView,
   ScrollView,
   StatusBar,
-  StyleSheet,
   Text,
   useColorScheme,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { Colors, Header } from 'react-native/Libraries/NewAppScreen';
 import Temp from './src/app/components/Temp';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({ children, title }: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): JSX.Element {
+const App = () => {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
@@ -66,48 +28,13 @@ function App(): JSX.Element {
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
         <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Egg">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            <Temp />
-            asdsa
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+        <View style={backgroundStyle}>
+          <Text>hello world</Text>
         </View>
+        <Temp />
       </ScrollView>
     </SafeAreaView>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
--- src/app/components/Temp.tsx
+++ src/app/components/Temp.tsx
@@ -1,8 +1,121 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import MapView from 'react-native-maps';
+import { View, StyleSheet, PermissionsAndroid, Platform } from 'react-native';
+import { PROVIDER_GOOGLE } from 'react-native-maps/lib/ProviderConstants';
+import Geolocation from 'react-native-geolocation-service';
+
+const getCurrentPosition = (
+  handlePosition: (pos: Geolocation.GeoPosition) => void,
+  handleError: (err: Geolocation.GeoError) => void = console.log,
+) => {
+  Geolocation.getCurrentPosition(handlePosition, handleError, {
+    enableHighAccuracy: true,
+    timeout: 15000,
+    maximumAge: 10000,
+  });
+};
+
+/**
+ * Requests for location permission based on platform OS.
+ *
+ * @param retry continuously requests for location permission if true
+ * @returns whether location permission has been granted
+ */
+const requestForLocationPermission = async (retry?: boolean) => {
+  if (Platform.OS === 'android') {
+    const result = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Taxi Fair Location Permission',
+        message:
+          'Taxi Fair needs access to your location ' +
+          'so you can view taxi routes.',
+        buttonNeutral: 'Ask Me Later',
+        buttonNegative: 'Cancel',
+        buttonPositive: 'OK',
+      },
+    );
+    if (retry && result !== PermissionsAndroid.RESULTS.GRANTED) {
+      requestForLocationPermission();
+    }
+    return result === PermissionsAndroid.RESULTS.GRANTED;
+  } else if (Platform.OS === 'ios') {
+    const result = await Geolocation.requestAuthorization('whenInUse');
+    if (retry && result !== 'granted') {
+      requestForLocationPermission();
+    }
+    return result === 'granted';
+  }
+};
+
+interface Location {
+  latitude: number;
+  longitude: number;
+  latitudeDelta: number;
+  longitudeDelta: number;
+}
 
 const Temp = () => {
-  return <Text>hello</Text>;
+  const [location, setLocation] = useState<Location>({
+    latitude: 1.30457,
+    longitude: 103.772392,
+    latitudeDelta: 0.001,
+    longitudeDelta: 0.001,
+  });
+
+  useEffect(() => {
+    // TODO: add check for iOS
+    PermissionsAndroid.check(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+    ).then(hasPermission => {
+      if (hasPermission) {
+        getCurrentPosition(pos =>
+          setLocation({
+            latitude: pos.coords.latitude,
+            longitude: pos.coords.longitude,
+            latitudeDelta: pos.coords.accuracy / 1000,
+            longitudeDelta: pos.coords.accuracy / 1000,
+          }),
+        );
+      } else {
+        requestForLocationPermission().then(permissionGranted => {
+          if (permissionGranted) {
+            getCurrentPosition(pos =>
+              setLocation({
+                latitude: pos.coords.latitude,
+                longitude: pos.coords.longitude,
+                latitudeDelta: pos.coords.accuracy / 100000,
+                longitudeDelta: pos.coords.accuracy / 100000,
+              }),
+            );
+          }
+        });
+      }
+    });
+  }, []);
+
+  console.log({ location });
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE}
+        style={styles.map}
+        initialRegion={location}
+      />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: '100%',
+    width: '100%',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
 
 export default Temp;
